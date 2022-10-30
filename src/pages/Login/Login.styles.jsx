1d9da126235bfd3f71a6f38cd090ccd2a480d1db
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-10-30 20:29:41 -0300

    feat: add component Container and style

---
+++ src/pages/Login/Login.styles.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+
+
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    position: absolute;
+    align-items: center;
+    background-color: blue;
+    flex-direction: column;
+    justify-content: center;
+
+  h1{
+    color:red;
+    font-family: 'Roboto Serif', serif;
+  }
+`;
