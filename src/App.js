f25a11c83b0ed491307288b4f8cd2be20a32ff6c
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-10-31 23:42:30 -0300

    feat: add new route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Contact from "./pages/contact/Contact";
 
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
 
 
