70d60db5661d6fbb78b1af84934cb2ab6a6eb0f8
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-11-13 08:48:46 -0300

    feat: add themeProvider

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,25 +3,35 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import NavBar from '../NavBar';
 import MenuMobail from '../MenuMobail';
 
 
 
 export default function Header() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#0089D1',
+            },
+        },
+    });
     return (
-
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography className='title' variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Energiar
-                    </Typography>
-                    <NavBar />
-                    <MenuMobail />
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <ThemeProvider theme={theme}>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" color='primary'>
+                    <Toolbar>
+                        <Typography className='title' variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Energiar
+                        </Typography>
+                        <NavBar />
+                        <MenuMobail />
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </ThemeProvider>
 
 
     );
