0ecd85ae26382648792a0b6c3dbaeca67a1be9ad
Cleiton Sobrinho <cleiton1.life@gmail.com>
2023-10-04 20:13:14 -0300

    feat: add change in NavBar Component

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,29 +1,27 @@
-import { Link } from "react-router-dom";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { BiLogoGmail } from "react-icons/bi";
+import { BsWhatsapp } from "react-icons/bs";
 import { NavStyle } from "./NavBar.styles";
 
-
-
-
-
 const NavBar = () => {
-    return (
-        <NavStyle>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contato</Link>
-                </li>
-                <li>
-                    <Link to="/login">Sair</Link>
-                </li>
-            </ul>
-        </NavStyle>
-    )
+  return (
+    <NavStyle>
+      <ul>
+        <li>
+          <AiOutlineInstagram className="icons" size={25} />
+        </li>
+        <li>
+          <BiLogoGmail className="icons" size={25} />
+        </li>
+        <li>
+          <BsWhatsapp className="icons" size={25} />
+        </li>
+      </ul>
+      <div>
+        <button>Agende agora</button>
+      </div>
+    </NavStyle>
+  );
 };
-
-
-
 
 export default NavBar;
--- src/components/nav-bar/NavBar.styles.jsx
+++ src/components/nav-bar/NavBar.styles.jsx
@@ -1,25 +1,47 @@
 import styled from "styled-components";
 
-
 export const NavStyle = styled.nav`
-    ul{
-        display: flex;
-       
+  display: flex;
+  align-items: center;
+  gap: 30px;
+
+  button {
+    height: 32px;
+    width: 150px;
+    border-radius: 10px;
+    font-size: 18px;
+    font-weight: 100;
+    border-bottom: 0px;
+    box-shadow: 0px;
+    cursor: pointer;
+
+    &:hover {
+      color: #008cff;
+      cursor: pointer;
     }
-    li{
-        
-        list-style: none;
-        
-       
+  }
+  ul {
+    display: flex;
+    gap: 20px;
+  }
+  li {
+    display: flex;
+    position: 50%;
+    list-style: none;
+    background-color: white;
+    height: 32px;
+    width: 32px;
+    border-radius: 50%;
+  }
+
+  .icons {
+    margin-top: 5px;
+    margin: auto;
+    color: #1f1f1f;
+
+    &:hover {
+      color: #008cff;
+      cursor: pointer;
     }
-   li a{
-    font-family: "Roboto","Helvetica","Arial",sans-serif;
-        text-decoration:none;
-        color: white;
-        margin-right:20px;
-        font-weight: 500;
-        font-size: 1.25rem;
-        line-height: 1.6;
-        letter-spacing: 0.0075em;
-    }
+  }
 `;
