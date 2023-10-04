87f21e74d15966758c23ef2cb83d4e90a6ad5a2d
Cleiton Sobrinho <cleiton1.life@gmail.com>
2023-10-04 20:19:15 -0300

    chore: add the react-icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Contact from "./pages/contact/Contact";
 
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+
 
 
 
