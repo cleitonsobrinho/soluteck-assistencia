285f1908b52e6585655dc33c191f62acc8efb03e
Cleiton Sobrinho <cleiton1.life@gmail.com>
2023-10-04 20:14:16 -0300

    feat: add change in Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
+import Description from "../../components/description/description";
 import Header from "../../components/header/Header";
+import SectionSlides from "../../section/section";
 import { ContainerHome } from "./Home.styles";
 
 const Home = () => {
-    return (
-        <ContainerHome>
-            <Header />
-        </ContainerHome>
-    )
-}
-
-
+  return (
+    <ContainerHome>
+      <Header />
+      <SectionSlides />
+      <Description />
+    </ContainerHome>
+  );
+};
 
 export default Home;
--- src/pages/Home/Home.styles.jsx
+++ src/pages/Home/Home.styles.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-
 export const ContainerHome = styled.div`
-
-
+  margin: 0;
+  padding: 0;
+  font-family: "Poppins", sans-serif;
 `;
